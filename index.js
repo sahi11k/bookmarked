f31e6c63c3955b89526b349fbf68ff2223cf4b33
Sahil Kumar <sahil511kumar@gmail.com>
2025-01-22 02:06:58 +0530

    add books card

--- index.js
+++ index.js
@@ -1,23 +1,65 @@
-const booksToRead = [];
+(() => {
+  const booksToRead = [];
+  const $bookList = document.getElementById("book-list");
 
-function Book(title, author, pages, read) {
-  this.title = title;
-  this.author = author;
-  this.pages = pages;
-  this.read = read;
-}
+  function Book(title, author, pages, status) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.status = status;
+  }
 
-function addBookToLibrary(name, author, pages) {
-  const book = new Book(name, author, pages, false);
-  booksToRead.push(book);
-}
+  function addBookToLibrary(name, author, pages) {
+    const book = new Book(name, author, pages, false);
+    booksToRead.push(book);
+  }
 
-function displayBooks() {}
+  function displayBooks() {
+    booksToRead.forEach((book, i) => {
+      const bookCard = createBookCard(book, i);
+      $bookList.appendChild(bookCard);
+    });
+  }
 
-addBookToLibrary("The Great Gatsby", "F. Scott Fitzgerald", 180);
-addBookToLibrary("To Kill a Mockingbird", "Harper Lee", 281);
-addBookToLibrary("1984", "George Orwell", 328);
-addBookToLibrary("Pride and Prejudice", "Jane Austen", 279);
-addBookToLibrary("The Catcher in the Rye", "J.D. Salinger", 224);
+  function createBookCard(book, index) {
+    const card = document.createElement("div");
+    const cardId = `book-${index}`;
+    card.id = cardId;
+    card.className = "card";
 
-console.log(booksToRead);
+    const cardContent = document.createElement("div");
+    cardContent.innerHTML = `
+      <h3 class="card-title">${book.title}</h3>
+      <span class="card-author">${book.author}</span>
+    `;
+
+    const cardFooter = document.createElement("div");
+    cardFooter.className = "card-footer";
+
+    const readBtn = document.createElement("button");
+    readBtn.className = "read-btn";
+    readBtn.textContent = book.status ? "Completed" : "Mark Completed";
+    readBtn.addEventListener("click", markAsRead);
+
+    const removeBtn = document.createElement("button");
+    removeBtn.className = "remove-btn";
+    removeBtn.textContent = "Remove";
+    removeBtn.addEventListener("click", removeBook);
+
+    cardFooter.append(readBtn, removeBtn);
+    card.append(cardContent, cardFooter);
+    return card;
+  }
+
+  function markAsRead(e) {}
+
+  function removeBook(e) {}
+
+  addBookToLibrary("The Great Gatsby", "F. Scott Fitzgerald", 180);
+  addBookToLibrary("To Kill a Mockingbird", "Harper Lee", 281);
+  addBookToLibrary("1984", "George Orwell", 328);
+  addBookToLibrary("Pride and Prejudice", "Jane Austen", 279);
+  addBookToLibrary("The Catcher in the Rye", "J.D. Salinger", 224);
+
+  displayBooks();
+})();
