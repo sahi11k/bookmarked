26e0f0207602563d2284f3d36269db7655821960
Sahil Kumar <sahil511kumar@gmail.com>
2025-01-26 03:02:02 +0530

    add ui fixes and logics

--- index.js
+++ index.js
@@ -1,11 +1,39 @@
 (() => {
-  const booksToRead = [];
+  let booksToRead = [
+    {
+      title:
+        "The Lord of the Rings : The Fellowship of the  Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.",
+      author: "J.R.R. Tolkien",
+      status: false,
+      id: 1,
+    },
+    {
+      title: "The Great Gatsby",
+      author: "F. Scott Fitzgerald asdfasdfasdf asdfasdfasdfasdf",
+      status: false,
+      id: 2,
+    },
+  ];
 
+  // book list wrapper element
   const $bookList = document.querySelector("#book-list");
+
+  // modal elements
   const $modal = document.querySelector("#modal");
-  const $addBookButton = document.querySelector("#add-book-btn");
-  const $closeModalButton = document.querySelector(".close-modal-btn");
+  const $showModalButton = document.querySelector("#show-modal-btn");
   const $modalBackdrop = document.querySelector(".modal-backdrop");
+
+  // form elements
+  const $form = document.querySelector("#add-book-form");
+
+  class Book {
+    constructor(title, author, status = false) {
+      this.title = title;
+      this.author = author;
+      this.status = status;
+      this.id = booksToRead.length + 1;
+    }
+  }
 
   // modal functions
   function openModal() {
@@ -14,46 +42,73 @@
   }
 
   function closeModal() {
+    $form.reset();
     $modal.close();
     $modalBackdrop.style.display = "none";
   }
 
   init();
 
+  function formSubmitHandler() {
+    const formData = new FormData($form);
+    const title = formData.get("book-title");
+    const author = formData.get("book-author");
+    addBookToLibrary(title, author);
+    closeModal();
+  }
+
+  function modalActionsHandler(e) {
+    const classList = e.target.classList;
+    if (classList.contains("ok-btn")) {
+      formSubmitHandler();
+    } else if (classList.contains("cancel-btn")) {
+      closeModal();
+    }
+  }
+
   function init() {
-    $addBookButton.addEventListener("click", openModal);
-    $closeModalButton.addEventListener("click", closeModal);
+    $showModalButton.addEventListener("click", openModal);
+    $modal.addEventListener("click", modalActionsHandler);
+    displayBooks();
   }
 
-  function Book(title, author, pages, status) {
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.status = status;
-  }
-
-  function addBookToLibrary(name, author, pages) {
-    const book = new Book(name, author, pages, false);
+  function addBookToLibrary(title, author) {
+    const book = new Book(title, author, false);
     booksToRead.push(book);
+    const bookCard = createBookCard(book);
+    $bookList.appendChild(bookCard);
   }
 
   function displayBooks() {
-    booksToRead.forEach((book, i) => {
-      const bookCard = createBookCard(book, i);
-      $bookList.appendChild(bookCard);
+    const fragment = document.createDocumentFragment();
+    booksToRead.forEach((book) => {
+      const bookCard = createBookCard(book);
+      fragment.appendChild(bookCard);
     });
+    $bookList.appendChild(fragment);
   }
 
-  function createBookCard(book, index) {
+  function createBookCard(book) {
     const card = document.createElement("div");
-    const cardId = `book-${index}`;
-    card.id = cardId;
+    card.id = `book-${book.id}`;
     card.className = "card";
 
     const cardContent = document.createElement("div");
+    cardContent.className = "card-content";
     cardContent.innerHTML = `
-      <h3 class="card-title">${book.title}</h3>
-      <span class="card-author">${book.author}</span>
+    <div class="book-title-wrapper">
+      <h3 class="book-title" title="${book.title}">${truncateString(
+      book.title,
+      50
+    )}</h3>
+      <div class="book-status-icon">
+        <img src="/assets/check-outlined.svg" alt="check-circle" />
+      </div>
+    </div>
+    <div class="book-author" title="${book.author}">${truncateString(
+      book.author,
+      25
+    )}</div>
     `;
 
     const cardFooter = document.createElement("div");
@@ -61,8 +116,8 @@
 
     const readBtn = document.createElement("button");
     readBtn.className = "secondary-btn read-btn";
-    readBtn.textContent = book.status ? "Completed" : "Mark Completed";
-    readBtn.addEventListener("click", markAsRead);
+    readBtn.textContent = book.status ? "Mark Unread" : "Mark Read";
+    readBtn.addEventListener("click", changeBookStatus);
 
     const removeBtn = document.createElement("button");
     removeBtn.className = "secondary-btn remove-btn";
@@ -74,15 +129,36 @@
     return card;
   }
 
-  function markAsRead(e) {}
+  function changeBookStatus(e) {
+    const card = e.target.closest(".card");
+    if (!card) return;
 
-  function removeBook(e) {}
+    let newBookStatus = null;
+    booksToRead = booksToRead.map((book) => {
+      if (`book-${book.id}` === card.id) {
+        newBookStatus = !book.status;
+        book.status = newBookStatus;
+      }
+      return book;
+    });
 
-  addBookToLibrary("The Great Gatsby", "F. Scott Fitzgerald", 180);
-  addBookToLibrary("To Kill a Mockingbird", "Harper Lee", 281);
-  addBookToLibrary("1984", "George Orwell", 328);
-  addBookToLibrary("Pride and Prejudice", "Jane Austen", 279);
-  addBookToLibrary("The Catcher in the Rye", "J.D. Salinger", 224);
+    card.querySelector(".book-status-icon").style.display = newBookStatus
+      ? "block"
+      : "none";
 
-  displayBooks();
+    card.querySelector(".read-btn").textContent = newBookStatus
+      ? "Mark Unread"
+      : "Mark Read";
+  }
+
+  function removeBook(e) {
+    const card = e.target.closest(".card");
+    if (!card) return;
+    booksToRead = booksToRead.filter((book) => `book-${book.id}` !== card.id);
+    card.remove();
+  }
+
+  function truncateString(str, maxLength) {
+    return str.length > maxLength ? str.slice(0, maxLength) + "..." : str;
+  }
 })();
